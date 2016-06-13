696601722e0de702dba9e39f57db881ef139af11
JohnLim <johnlimjsj@gmail.com>
2016-06-13 12:49:25 -0700

    initial commit

---
+++ Code/KK2_testcode/KK2_test1/KK2_test1.ino
@@ -0,0 +1,18 @@
+#define red_AUX 10
+#define orange_RUD 9
+#define yellow_THR 6
+#define green_ELE 5
+#define blue_AIL 3
+
+void setup() {
+  pinMode(red_AUX, OUTPUT);
+  pinMode(orange_RUD, OUTPUT);
+  pinMode(yellow_THR, OUTPUT);
+  pinMode(green_ELE, OUTPUT);
+  pinMode(blue_AIL, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
